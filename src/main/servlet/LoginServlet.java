80f50ae5c168e24effbe820102b517735d416347
huangjunlong <Admin@huangjunlong>
2017-03-16 17:42:31 +0800

    提交

---
+++ src/main/servlet/LoginServlet.java
@@ -0,0 +1,64 @@
+package main.servlet;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "login" , urlPatterns = { "/login" })
+@SuppressWarnings("unchecked")
+public class LoginServlet extends HttpServlet{
+	
+	private static final long serialVersionUID = -7232348095464908546L;
+
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException{
+		System.out.println("登陆方法已经进来");
+		System.out.println("循环输出cookie值");
+		Cookie [] cookies = req.getCookies();
+		if(cookies != null){
+			for(Cookie c : cookies){
+				System.out.println("name : "+c.getName()+"---- value : "+c.getValue());
+			}
+		}
+		System.out.println("输出session值");
+		if(req.getSession() != null){			
+			System.out.println(req.getSession().getId());
+		}
+		
+		String username = req.getParameter("username");
+		String password = req.getParameter("password");
+		System.out.println("用户名 : "+username+"----密码 : "+password);
+		System.out.println("开始验证用户密码是否正确");
+		
+		
+		Map<String,String> userMap = (Map<String, String>) this.getServletContext().getAttribute("userMap");
+		if(userMap.containsKey(username)&&userMap.get(username).equals(password)){
+			System.out.println("登录成功");
+			//resp.getWriter().print("登录成功");
+		}else{
+			System.out.println("登录失败");
+			//resp.getWriter().print("登录失败");
+		}
+				
+		System.out.println("登陆方法已经结束");
+	}
+	
+	public void doPost(HttpServletRequest req, HttpServletResponse resp){
+		
+	}
+	
+	public void init(){
+		/**
+		 * 初始化用户名密码
+		 */
+		Map<String,String> userMap = new HashMap<String,String>();
+		userMap.put("huangjunlong","123");
+		userMap.put("geek","321");
+		this.getServletContext().setAttribute("userMap", userMap);
+	}
+}
