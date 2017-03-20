ca44c1413b007936095eb09380df8bad882a8112
huangjunlong <Admin@huangjunlong>
2017-03-20 17:44:46 +0800

    添加passwrod reverse 的filter

--- src/main/filter/PasswordReverseFilter.java
+++ src/main/filter/PasswordReverseFilter.java
@@ -11,29 +11,29 @@ import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 
-import main.wrapper.TrimWrapper;
+import main.wrapper.PasswordReverseWrapper;
 
-@WebFilter(filterName="TrimFilter",urlPatterns="/*")
-public class TrimFilter implements Filter{
+@WebFilter(filterName="PasswordReverseFilter",urlPatterns="/*")
+public class PasswordReverseFilter implements Filter{
 
 	@Override
 	public void destroy() {
-		System.out.println("trim filter destory");
+		System.out.println("password reverse filter destory");		
 	}
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain)
 			throws IOException, ServletException {
-		System.out.println("trim filter working");
-		//request.para
-		TrimWrapper wrapper = new TrimWrapper((HttpServletRequest)request);
-		filterChain.doFilter(wrapper, response);
-		System.out.println("trim filter end");
+		System.out.println("password reverse filter working");
+		PasswordReverseWrapper prw = new PasswordReverseWrapper((HttpServletRequest)request);
+		filterChain.doFilter(prw, response);
+		System.out.println("password reverse filter end");
+		
 	}
 
 	@Override
 	public void init(FilterConfig arg0) throws ServletException {
-		System.out.println("trim filter init");
+		System.out.println("password reverse filter init");	
 	}
-	
+
 }
---
+++ src/main/wrapper/PasswordReverseWrapper.java
@@ -0,0 +1,20 @@
+package main.wrapper;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+
+public class PasswordReverseWrapper extends HttpServletRequestWrapper{
+
+	public PasswordReverseWrapper(HttpServletRequest request) {
+		super(request);
+	}
+	
+    public String getParameter(String name) {
+    	if(name.trim().equals("password")){
+    		StringBuffer sb = new StringBuffer(this.getRequest().getParameter(name).trim());
+    		return sb.reverse().toString();
+    	}
+        return this.getRequest().getParameter(name).trim();
+    }
+	
+}
