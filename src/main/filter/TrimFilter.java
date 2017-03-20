4b31c09fb7b63f47431c19f46b7d57a0c4e9a170
huangjunlong <Admin@huangjunlong>
2017-03-20 17:19:29 +0800

    添加trimwrapper

--- src/main/filter/TrimFilter.java
+++ src/main/filter/TrimFilter.java
@@ -1,5 +1,38 @@
 package main.filter;
 
-public class TrimFilter {
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+
+import main.wrapper.TrimWrapper;
+
+@WebFilter(filterName="TrimFilter",urlPatterns="/*")
+public class TrimFilter implements Filter{
+
+	@Override
+	public void destroy() {
+		System.out.println("time filter destory");
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain)
+			throws IOException, ServletException {
+		System.out.println("time filter working");
+		//request.para
+		TrimWrapper wrapper = new TrimWrapper((HttpServletRequest)request);
+		filterChain.doFilter(wrapper, response);
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		System.out.println("time filter start");
+	}
 	
 }
---
+++ src/main/wrapper/TrimWrapper.java
@@ -0,0 +1,19 @@
+package main.wrapper;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+
+public class TrimWrapper extends HttpServletRequestWrapper{
+
+	public TrimWrapper(HttpServletRequest request) {
+		super(request);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+    public String getParameter(String name) {
+        return this.getRequest().getParameter(name).trim();
+    }
+    
+    //you can do more about parameters
+}
